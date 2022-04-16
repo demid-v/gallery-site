007aa628318b6d1ee077c0c9791bc50cd2ba23f8
Demid <vasilyev.demid.e@gmail.com>
2022-04-17 02:05:36 +0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Gallery from "./pages/Gallery";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/gallery" />}></Route>
+        <Route path="/gallery" element={<Gallery />}>
+          Галлерея
+        </Route>
+        <Route path="/about" element={<About />}>
+          Обо мне
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import { Nav, Navbar } from "react-bootstrap";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+import "../styles/header.scss";
+import { useState } from "react";
+import Image from "react-bootstrap/Image";
+import profilePic from "../assets/pfp.jpg";
+
+function Header() {
+  const [navState, setNavState] = useState(false);
+
+  function collapseNav() {
+    setNavState(false);
+  }
+
+  function setNavExpanded(expanded) {
+    setNavState(expanded);
+  }
+
+  return (
+    <Navbar
+      collapseOnSelect
+      expand=""
+      bg="dark"
+      variant="dark"
+      onToggle={setNavExpanded}
+      expanded={navState}
+      className="sticky-top px-2 pr-1"
+    >
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav" className="bg-dark">
+        <div className="profile-info">
+          <Image src={profilePic} roundedCircle className="pfp mt-4 mb-3" />
+          <p className="text-secondary mb-1">Васильев Демид</p>
+          <p className="text-secondary">vasilyev.demid.e@gmail.com</p>
+        </div>
+        <Nav onClick={collapseNav} className="mr-auto">
+          <Nav.Link as={Link} to="/gallery">
+            Галлерея
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about">
+            Обо мне
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default Header;
